d2dbae448b99253ccd2387d6337744c3834d4143
Andrew Huang <andrewhuang0917@yahoo.com>
2023-02-25 11:15:39 -0800

    Added random word retrieval

--- wordle.hs
+++ wordle.hs
@@ -1,25 +1,53 @@
 -- CPSC 312 - 2023 - Games in Haskell
 module Wordle where
 
+import System.IO
+import System.IO.Unsafe
+import System.Random
+
+------ Wordle Game -------
 -- To run it, try:
 -- ghci
 -- :load wordle
+-- play
 
------- Wordle Game -------
+-- Troubleshooting 
+-- If 'Could not find module `System.Random`
+--    brew install haskell-stack
+--    stack install random
+--    stack ghci 
+--    :load wordle
+--    play
 
-data State = State InternalState [Action]  -- internal_state available_actions
-         deriving (Ord, Eq)
+--- Retrieving a random word from our words.txt ---
+wordFilePath = "/Users/andrew/Documents/GitHub/cpsc312-project-1/words.txt"
+randomWord :: FilePath -> IO [String]
+randomWord path = do
+  contents <- readFile path
+  return $ words contents
 
--- EndOfGame is when the correct word is guessed
--- Otherwise, continue the game and update the state
-data Result = EndOfGame Double State    -- end of game: value, starting state
-            | ContinueGame State        -- continue with new state
-         deriving (Eq)
 
-type Game = Action -> State -> Result
+--- Check correctness of word and give feedback --- 
+wordle :: String -> String
+wordle = reverse
 
-type Player = State -> Action
 
-data Action = Action String                   -- a move for a player is just a String
-         deriving (Ord,Eq)
-type InternalState = ([Action],[Action])   -- (self,other)
+--- Main game function, creates a game object with a random word, enters into a while loop of guessing until correct ---
+play :: IO ()
+play = do
+  putStrLn "Welcome to Wordle++, created by Andrew, Keira, and Philly for CPSC 312!"
+
+  -- Retrieve a random word from the wordlist -- 
+  wordList <- randomWord wordFilePath
+  let wordListLen = 12792
+  let randomIndex = unsafePerformIO (getStdRandom (randomR (0, wordListLen - 1)))
+  let wordToGuess = (wordList !! randomIndex)
+
+  -- FOR DEBUGGING AND DEVELOPING ONLY, REMOVE LATER 
+  putStrLn "The word to guess is"
+  putStrLn wordToGuess
+
+  -- Initiate the game
+
+  putStrLn "Done for now"
+
